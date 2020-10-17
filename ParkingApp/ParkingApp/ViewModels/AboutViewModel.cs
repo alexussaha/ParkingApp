7733bd4b370455e4cf6cc39a7cf59ffc6c5020ef
Usov Alexandr <ussov39@gmail.com>
2020-10-17 19:29:41 +0300

    Дизайн

--- ParkingApp/ParkingApp/ViewModels/AboutViewModel.cs
+++ ParkingApp/ParkingApp/ViewModels/AboutViewModel.cs
@@ -9,8 +9,8 @@ namespace ParkingApp.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+            Title = "";
+            
         }
 
         public ICommand OpenWebCommand { get; }
