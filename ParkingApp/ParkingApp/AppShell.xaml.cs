9eb06da3ea65b3915d37a2335a59288a80bf79ab
Usov Alexandr <ussov39@gmail.com>
2020-10-17 16:53:43 +0300

    Форма входа

--- ParkingApp/ParkingApp/AppShell.xaml.cs
+++ ParkingApp/ParkingApp/AppShell.xaml.cs
@@ -15,5 +15,10 @@ namespace ParkingApp
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+
     }
 }
